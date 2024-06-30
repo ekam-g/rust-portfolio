42384022ace5bd78df15ac55503692c9e02803d9
carghai <carghai@proton.me>
2024-06-29 21:16:06 -0400

    works

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
-#[macro_use] extern crate rocket;
-
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
-}
+use crate::pages::index;
+use rocket_dyn_templates::Template;
+pub mod pages;
+#[macro_use]
+extern crate rocket;
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build()
+        .mount("/", routes![index])
+        .attach(Template::custom(|engines| {
+            pages::customize(&mut engines.handlebars);
+        }))
 }
---
+++ src/pages/mod.rs
@@ -0,0 +1,42 @@
+use rocket::get;
+
+use rocket_dyn_templates::{context, handlebars, Template};
+
+use self::handlebars::{Handlebars, JsonRender};
+#[get("/")]
+pub fn index() -> Template {
+    Template::render("home", context! {Test : "Yo"})
+}
+
+fn wow_helper(
+    h: &handlebars::Helper<'_>,
+    _: &handlebars::Handlebars,
+    _: &handlebars::Context,
+    _: &mut handlebars::RenderContext<'_, '_>,
+    out: &mut dyn handlebars::Output,
+) -> handlebars::HelperResult {
+    if let Some(param) = h.param(0) {
+        out.write("<b><i>")?;
+        out.write(&param.value().render())?;
+        out.write("</b></i>")?;
+    }
+
+    Ok(())
+}
+pub fn customize(hbs: &mut Handlebars) {
+    hbs.register_helper("wow", Box::new(wow_helper));
+    hbs.register_template_string(
+        "hbs/about.html",
+        r#"
+        {{#*inline "page"}}
+
+        <section id="about">
+          <h1>About - Here's another page!</h1>
+        </section>
+
+        {{/inline}}
+        {{> hbs/layout}}
+    "#,
+    )
+    .expect("valid HBS template");
+}
