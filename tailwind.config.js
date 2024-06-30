f7ead3cdea3fe669b997c20d16ffc2783ff8d707
carghai <carghai74@gmail.com>
2024-06-30 18:22:18 -0400

    took me way too long

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./templates/**/*.{html,js}"],
+    content: ["./templates/**/*.{html,hbs}"],
     theme: {
+      animation: [
+        'fadeIn { animation: fadeIn 1s; }',
+        '@keyframes fadeIn { 0% { opacity: 0; } 100% { opacity: 1; } }'
+      ],
       extend: {},
     },
     plugins: [],
