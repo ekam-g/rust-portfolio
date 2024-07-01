508f793de09e48f6cac3e40137cd8a25330ddfb6
carghai <carghai74@gmail.com>
2024-06-30 22:51:43 -0400

    fixed errors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,6 @@
 module.exports = {
     content: ["./templates/**/*.{html,hbs}"],
     theme: {
-      animation: [
-        'fadeIn { animation: fadeIn 1s; }',
-        '@keyframes fadeIn { 0% { opacity: 0; } 100% { opacity: 1; } }'
-      ],
       extend: {},
     },
     plugins: [],
